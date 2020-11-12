c50bb9e1e6fc89e25b937c3b41fd1acfc9653489
Sanchit <sanchitgupta456@gmail.com>
2020-11-12 16:11:04 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,72 @@
+const data  ={
+    products: [
+        {
+            _id: 1,
+            name:'Nike Slim Shirt',
+            category:'Shirts',
+            image:'./images/p1.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'High Quality Product'
+        },
+        {
+            _id: 2,
+            name:'Adidas',
+            category:'Shirts',
+            image:'./images/p2.jpg',
+            price:80,
+            brand:'Nike',
+            rating:5,
+            numReviews:10,
+            description:'High Quality Product'
+        },
+        {
+            _id: 3,
+            name:'Fit Shirt',
+            category:'Shirts',
+            image:'./images/p3.jpg',
+            price:100,
+            brand:'Nike',
+            rating:1.5,
+            numReviews:12,
+            description:'High Quality Product'
+        },
+        {
+            _id: 4,
+            name:'Super Pant',
+            category:'Pants',
+            image:'./images/p4.jpg',
+            price:90,
+            brand:'Nike',
+            rating:3.5,
+            numReviews:15,
+            description:'High Quality Product'
+        },
+        {
+            _id: 5,
+            name:'Fit Pant',
+            category:'Pants',
+            image:'./images/p5.jpg',
+            price:1200,
+            brand:'Nike',
+            rating:4.0,
+            numReviews:20,
+            description:'High Quality Product'
+        },
+        {
+            _id: 6,
+            name:'Best Pant',
+            category:'Pants',
+            image:'./images/p5.jpg',
+            price:1200,
+            brand:'Nike',
+            rating:4.4,
+            numReviews:10,
+            description:'High Quality Product'
+        },
+    ]
+}
+
+export default data;
