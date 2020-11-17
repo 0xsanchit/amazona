40a0ecfcd3ae36752696d4ca89a81da1920bf263
Sanchit <sanchitgupta456@gmail.com>
2020-11-17 16:21:31 +0530

    Created the Product Screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
-import data from './data';
+import {BrowserRouter,Route} from 'react-router-dom'
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="grid-container">
             <header className="row">
                 <div>
@@ -14,50 +17,12 @@ function App() {
                 </div>
             </header>
             <main>
-                <div className="row center">
-                  {
-                    data.products.map((product) => (
-                      <div key={product._id} className="card">
-                        <a href={`\products/${product._id}`}>
-                            <img className="medium" src={product.image} alt={product.name} />
-                        </a>
-                        <div className="card-body">
-                            <a href="product.html">
-                                <h2>{product.name}</h2>
-                            </a>
-                            <div className="rating">
-                                <span>
-                                    <i className="fa fa-star"></i>
-                                </span>
-                                <span>
-                                    <i className="fa fa-star"></i>
-                                </span>
-                                <span>
-                                    <i className="fa fa-star"></i>
-                                </span>
-                                <span>
-                                    <i className="fa fa-star"></i>
-                                </span>
-                                <span>
-                                    <i className="fa fa-star"></i>
-                                </span>
-                            </div>
-                            <div className="price">
-                                ${product.price}
-                            </div>
-                        </div>
-                    </div>       
-                    ))
-                  }             
-                </div>
-                <ul>
-                    <li>Product 1</li>
-                    <li>Product 2</li>
-                    <li>Product 3</li>
-                </ul>
+              <Route path="/product/:id" component={ProductScreen}></Route>
+              <Route path="/" component={HomeScreen} exact></Route>
             </main>
             <footer className="row center">All Rights Reserved</footer>
         </div>
+        </BrowserRouter>
   );
 }
 
--- src/data.js
+++ src/data.js
@@ -1,69 +1,75 @@
-const data  ={
+const data  = {
     products: [
         {
             _id: 1,
             name:'Nike Slim Shirt',
             category:'Shirts',
-            image:'./images/p1.jpg',
+            image:'../images/p1.jpg',
             price:120,
             brand:'Nike',
             rating:4.5,
             numReviews:10,
+            countInStock:0,
             description:'High Quality Product'
         },
         {
             _id: 2,
             name:'Adidas',
             category:'Shirts',
-            image:'./images/p2.jpg',
+            image:'../images/p2.jpg',
             price:80,
             brand:'Nike',
             rating:5,
             numReviews:10,
+            countInStock:1,
             description:'High Quality Product'
         },
         {
             _id: 3,
             name:'Fit Shirt',
             category:'Shirts',
-            image:'./images/p3.jpg',
+            image:'../images/p3.jpg',
             price:100,
             brand:'Nike',
             rating:1.5,
             numReviews:12,
+            countInStock:2,
             description:'High Quality Product'
         },
         {
             _id: 4,
             name:'Super Pant',
             category:'Pants',
-            image:'./images/p4.jpg',
+            image:'../images/p4.jpg',
             price:90,
             brand:'Nike',
             rating:3.5,
             numReviews:15,
+            countInStock:3,
             description:'High Quality Product'
         },
         {
             _id: 5,
             name:'Fit Pant',
             category:'Pants',
-            image:'./images/p5.jpg',
+            image:'../images/p5.jpg',
             price:1200,
             brand:'Nike',
             rating:4.0,
             numReviews:20,
+            countInStock:4,
             description:'High Quality Product'
         },
         {
             _id: 6,
             name:'Best Pant',
             category:'Pants',
-            image:'./images/p5.jpg',
+            image:'../images/p5.jpg',
             price:1200,
             brand:'Nike',
             rating:4.4,
             numReviews:10,
+            countInStock:5,
             description:'High Quality Product'
         },
     ]
